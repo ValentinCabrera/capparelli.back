52fa748347e45efd677b72a52cd9728d3d799c94
ValentinCabrera <valentincabrera2003@gmail.com>
2023-11-08 11:38:54 -0300

    user models, admin

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
 
-# Create your models here.
+    def __str__(self):
+        return self.name + ' ' + self.surname
